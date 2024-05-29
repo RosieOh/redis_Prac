03c815efc8134504cf082ca29b2ec0ee315c7c0e
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-29 19:44:48 +0900

    ✨FEAT : 선착순 쿠폰 발행을 위한 이벤트 Enumeration 타입 지정

---
+++ src/main/java/com/redis_prac/constant/Event.java
@@ -0,0 +1,14 @@
+package com.redis_prac.constant;
+
+import lombok.Getter;
+
+@Getter
+public enum Event {
+    CHICKEN("치킨");
+
+    private String name;
+
+    Event(String name) {
+        this.name = name;
+    }
+}

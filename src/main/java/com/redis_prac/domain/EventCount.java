3f0be166238842181535ffd498639d1b3acfdcff
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-29 19:47:10 +0900

    ✨FEAT : Event 시작과 종료 지점 도메인 설정

---
+++ src/main/java/com/redis_prac/domain/EventCount.java
@@ -0,0 +1,28 @@
+package com.redis_prac.domain;
+
+import com.redis_prac.constant.Event;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EventCount {
+
+    private Event event;
+    private int limit;
+
+    private static final int END = 0;
+
+    public EventCount(Event event, int limit) {
+        this.event = event;
+        this.limit = limit;
+    }
+
+    public synchronized void decrease() {
+        this.limit--;
+    }
+
+    public boolean end() {
+        return this.limit == END;
+    }
+}

f5a255cba938eb213b7c72e0c5fa697b88c6960b
RosieOh <20172207@gm.hannam.ac.kr>
2024-05-29 19:44:21 +0900

    ✨FEAT : Redis Config 로직 작성

---
+++ src/main/java/com/redis_prac/config/RedisConfig.java
@@ -0,0 +1,10 @@
+package com.redis_prac.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedisConfig {
+    private
+}

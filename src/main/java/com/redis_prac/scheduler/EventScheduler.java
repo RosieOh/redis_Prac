f3d4bfd6b66912661508e1b5f76779c25cff469d
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-01 17:02:24 +0900

    ✨FEAT : 선착순 100명에게 기프티콘 30개 증정 이벤트 서비스 로직 구현

--- src/main/java/com/redis_prac/scheduler/EventScheduler.java
+++ src/main/java/com/redis_prac/scheduler/EventScheduler.java
@@ -1,13 +1,27 @@
 package com.redis_prac.scheduler;
 
-import com.redis_prac.domain.Gifticon;
+import com.redis_prac.constant.Event;
+import com.redis_prac.service.GifticonService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 @RequiredArgsConstructor
 public class EventScheduler {
+    private final GifticonService gifticonService;
+
+    @Scheduled(fixedDelay = 1000)
+    private void chickenEventScheduler() {
+        if(gifticonService.validEnd()) {
+            log.info("==========선착순 이벤트가 종료되었습니다. ==========");
+            return;
+        }
+
+        gifticonService.publish(Event.CHICKEN);
+        gifticonService.getOrder(Event.CHICKEN);
+    }
 
 }

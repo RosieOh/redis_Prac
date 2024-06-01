6b128e2e23b96da1d1426083dc1e58613ebba788
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-01 17:19:13 +0900

    ✨FEAT : Redis Bean 등록을 위한 설정파일 로직 작성

--- src/main/java/com/redis_prac/config/RedisConfig.java
+++ src/main/java/com/redis_prac/config/RedisConfig.java
@@ -1,10 +1,26 @@
 package com.redis_prac.config;
 
+import com.redis_prac.property.RedisProperty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @RequiredArgsConstructor
 public class RedisConfig {
-    private
+    private final RedisProperty redisProperty;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(redisProperty.getHost(), redisProperty.getPort());
+    }
+
+    @Bean
+    public RedisTemplate<?, ?> redisTemplate() {
+        redisTemplate().setConnectionFactory(redisConnectionFactory());
+        return redisTemplate();
+    }
 }

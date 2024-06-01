fb67b1962daa5d1bd9196e536201067024c1b818
RosieOh <20172207@gm.hannam.ac.kr>
2024-06-01 16:47:48 +0900

    ✨FEAT : 이벤트 출력을 실행할 스케줄러 설정

---
+++ src/main/java/com/redis_prac/scheduler/EventScheduler.java
@@ -0,0 +1,13 @@
+package com.redis_prac.scheduler;
+
+import com.redis_prac.domain.Gifticon;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+
+@Log4j2
+@Component
+@RequiredArgsConstructor
+public class EventScheduler {
+
+}
